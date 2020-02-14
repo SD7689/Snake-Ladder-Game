ba1ed75b1404919ccfae589efea7ee8b041c09bf
SD7689 <dewanganshivam89@gmail.com>
2020-02-14 15:28:41 +0530

    [Shivam]ADD.added a Random Dice No. Generator Function in Program

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-echo "<<=======WELCOME TO SNAKE & LADDER GAME IN SHELL SCRIPT=======>>"
+
+#UserCase2
+RandomDice()
+{
+num=$(($RANDOM%6+1))
+return $num
+}
+RandomDice
+echo "     Dice Thrown-"
+echo "OutCome of Dice is:" $num
