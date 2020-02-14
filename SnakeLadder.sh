35dd338c0a00d436d19873f0fec493de7e463f3d
SD7689 <dewanganshivam89@gmail.com>
2020-02-14 15:06:58 +0530

    [Shivam]ADD.added a Myboard Function to Tmaster

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,3 +1,48 @@
 #!/bin/sh
-echo "<<=======WELCOME TO SNAKE & LADDER GAME IN SHELL SCRIPT=======>>"
-echo "<<=======Start the Game======>>"
+echo
+echo "           <<~~~~~~ Play SnaKe & Ladder## ~~~~~>>"
+echo
+#UserCase_1
+MyBoard()
+{
+declare -A array
+num=101
+for((i=0; i<10; i++))
+do
+       if(($i%2==0))
+       then
+               for((j=0; j<10; j++))
+               do
+               num=$(($num-1))
+               array[$i,$j]=$num
+               done
+       else
+               for((j=9; j>=0; j--))
+               do
+               num=$(($num-1))
+               array[$i,$j]=$num
+               done
+       fi
+done
+echo "|----------------------------------------------------------|"
+       temp=101
+       for((i=0; i<10; i++))
+       do
+               for((j=0; j<10; j++))
+               do
+                       if(($temp<=10))
+                       then
+                               temp=$(($temp-1))
+                               echo -n "|  "
+                               echo -n "$((array[$i,$j])) |"
+                       else
+                               temp=$(($temp-1))
+                               echo -n "| "
+                               echo -n "$((array[$i,$j])) |"
+                       fi
+               done
+               echo
+               echo "|----------------------------------------------------------|"
+       done
+}
+MyBoard
