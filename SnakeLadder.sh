01b490c11e59a3394aefc4d6a084667cbaedee47
SD7689 <dewanganshivam89@gmail.com>
2020-02-15 12:24:17 +0530

    [Shivam]ADD. Added a Usercase3 Function to the program

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 start=0
-postion=0
+position=0
 
 #UserCase2
 RandomDice()
@@ -20,28 +20,24 @@ res=$?
 n=$((RANDOM%3+1))
 	if(($n==1))
 	then
-		echo "NO PLAY, Player Stays At Same postion."
+		echo "1. NO PLAY!!, Player Stays At Same position "
+		echo
 	elif(($n==2))
 	then
-		echo "Ladder is Present At Postion"
-		if(($postion+$res<100))
-		then
-			postion=$(($(($postion+$res))))
-		elif(($postion+$res==100))
-		then
-			echo "Player Wins."
-		fi
+		echo "2. Congo!!,Ladder is Present At Position  " 
+		echo
+			position=$(($position+$res))
 	elif(($n==3))
 	then
-		echo "Snake is Present"
-		if(($postion==0))
+		echo "3. Awww!!,Snake is Present At Position  "
+		echo
+		if((position>=$res))
 		then
-			echo "Player Stays at Same Postion" 
+			position=$(($position-$res))
 		else
-			postion=$(($(($postion+$res))))
+			position=0
 		 fi
 	fi
-	return $postion
 }
 Play
-echo Player postion is: $?
+
