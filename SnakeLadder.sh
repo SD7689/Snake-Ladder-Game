0316c7e5ae217ad39aa506c51006a7c8d1363fe0
SD7689 <dewanganshivam89@gmail.com>
2020-02-15 13:29:48 +0530

    [Shivam]ADD, addded a UserCase5 Function to the program

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,2 +1,60 @@
 #!/bin/sh
 echo "<<=======WELCOME TO SNAKE & LADDER GAME IN SHELL SCRIPT=======>>"
+#UserCase_1
+echo "Game Start"
+position=0
+#UserCase2
+RandomDice()
+{
+num=$(($RANDOM%6+1))
+return $num
+}
+RandomDice
+echo "     Dice Thrown-"
+echo "OutCome of Dice is:" $num
+
+#UserCase_3
+Play()
+{
+RandomDice
+res=$?
+n=$((RANDOM%3+1))
+	if(($n==1))
+	then
+		echo "1. NO PLAY, Player Stays At Same position - "$position
+		echo
+	elif(($n==2))
+	then
+		echo "2. Ladder is Present At Position - " $position
+		echo
+			position=$(($position+$res))
+			if(($position==100))
+			then
+				break
+			elif(($position>100))
+			then
+				position=$(($position-$res))
+			fi
+	elif(($n==3))
+	then
+		echo "3. Snake is Present At Position - " $position
+		echo
+		if((position>=$res))
+		then
+			position=$(($position-$res))
+		else
+			position=0
+		 fi
+	fi
+	return $position
+}
+#UserCase_4
+while((p<100))
+do
+	Play
+	p=$?
+done
+echo "<<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>"
+echo "\m/    Player Position is At : $p    \m/" 
+echo "            Player Wins"
+echo "<<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>"
